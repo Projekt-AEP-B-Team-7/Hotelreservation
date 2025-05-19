bd4e5bb17d6045e1cdcffe57712da88149a8256e
Albina <albina.ramesh@students.fhnw.ch>
2025-05-19 11:01:27 +0000

    __init_.py für model erstellt File __init_.py für model erstellt

--- data_access/hotel_data_access.py
+++ data_access/hotel_data_access.py
@@ -19,3 +19,28 @@ class HotelDataAccess(BaseDataAccess):
             return model.Hotel(hotel_id, name, stars)
         else:
             return None
+
+    def read_hotel_by_city(self, city: str) -> model.Hotel | None:
+        sql = """
+        SELECT HotelId, Name, City, Stars FROM Hotel WHERE City = ?;
+        """
+        params = tuple([city])
+        result = self.fetchone(sql, params)
+        if result:
+            hotel_id, name, city, stars = result
+            return model.Hotel(hotel_id, name, city, stars)
+        else:
+            return None
+    
+    def read_hotel_by_star(self, star: int) -> model.Hotel | None:
+        sql = """
+        SELECT HotelId, Name, City, Stars FROM Hotel WHERE Stars = ?;
+        """
+        params = tuple([star])
+        result = self.fetchone(sql, params)
+        if result:
+            hotel_id, name, city, stars = result
+            return model.Hotel(hotel_id, name, city, stars)
+        else:
+            return None
+    
