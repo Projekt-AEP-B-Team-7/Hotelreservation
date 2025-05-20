f1acf2bee0bee0a7a7783d4eccdb2157b2e0d994
luxaayini <luxaayini.ponnaiya@gmail.com>
2025-05-20 14:52:05 +0200

    Update room_type_data_access.py

--- data_access/room_type_data_access.py
+++ data_access/room_type_data_access.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import model
 from data_access.base_data_access import BaseDataAccess
 
-class RoomTypeDataAccess(BaseDataAccess):
+class RoomtypeDataAccess(BaseDataAccess):
     
     def __init__(self, db_path: str = None):
         super().__init__(db_path)
